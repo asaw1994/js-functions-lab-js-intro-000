3de3fcf282dc1f23a9db47f45c460c2f8fe00e74
Anna Saw <a2nasaw@gmail.com>
2020-07-08 22:04:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function happyHolidays()
   {return ("Happy holidays!")
 }
 
-function happyHolidaysTo(firstName){
-  return ('Happy holidays, ${firstName}!')
+function happyHolidaysTo(tName){
+  return ('Happy holidays, ${Name}!')
 }
